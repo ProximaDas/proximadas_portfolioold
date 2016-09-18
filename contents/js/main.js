1c5bfe54f3657b955ba77f790dd24c5116f40980
ProximaDas <prox.me@gmail.com>
2016-09-17 23:37:48 -0700

    Pushing website

---
+++ contents/js/main.js
@@ -0,0 +1,7 @@
+!function (a) {
+    "use strict";
+    /*Mobil menu selected to close menu*/
+    $("#bs-example-navbar-collapse-1 ul li").click(function () {
+        $("#bs-example-navbar-collapse-1").removeClass("in");
+    });
+}(this);
---
+++ contents/portfolio/js/portfolio-1.js
@@ -0,0 +1,65 @@
+(function($, window, document, undefined) {
+    'use strict';
+
+    $('#js-grid-juicy-projects').cubeportfolio({
+        filters: '.js-filters-juicy-projects',
+        loadMore: '#js-loadMore-juicy-projects',
+        loadMoreAction: 'click',
+        layoutMode: 'grid',
+        defaultFilter: '*',
+        animationType: 'quicksand',
+        gapHorizontal: 35,
+        gapVertical: 30,
+        gridAdjustment: 'responsive',
+        singlePageStickyNavigation: false,
+        mediaQueries: [{
+            width: 1500,
+            cols: 5
+        }, {
+            width: 1100,
+            cols: 4
+        }, {
+            width: 800,
+            cols: 3
+        }, {
+            width: 480,
+            cols: 2
+        }, {
+            width: 320,
+            cols: 1
+        }],
+        caption: 'overlayBottomReveal',
+        displayType: 'sequentially',
+        displayTypeSpeed: 80,
+
+        // lightbox
+        lightboxDelegate: '.cbp-lightbox',
+        lightboxGallery: false,
+        lightboxTitleSrc: 'data-title',
+        lightboxCounter: '<div class="cbp-popup-lightbox-counter">{{current}} of {{total}}</div>',
+
+        // singlePage popup
+        singlePageDelegate: '.cbp-singlePage',
+        singlePageDeeplinking: true,
+        singlePageStickyNavigation: false,
+        singlePageCounter: '',
+        singlePageCallback: function(url, element) {
+            // to update singlePage content use the following method: this.updateSinglePage(yourContent)
+            var t = this;
+
+            $.ajax({
+                    url: url,
+                    type: 'GET',
+                    dataType: 'html',
+                    timeout: 10000
+                })
+                .done(function(result) {
+                    t.updateSinglePage(result);
+                })
+                .fail(function() {
+                    t.updateSinglePage('AJAX Error! !');
+                });
+        },
+    });
+    
+})(jQuery, window, document);
